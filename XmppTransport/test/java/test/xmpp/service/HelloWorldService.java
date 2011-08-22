05d0a484a8944de3cd270e5b196b58e36870972b
Leon Doud <leon.doud@gmail.com>
2011-08-21 23:34:42 -0400

    Changed service endpoint to SOAP 1.2 from SOAP 1.1

--- XmppTransport/test/java/test/xmpp/service/HelloWorldService.java
+++ XmppTransport/test/java/test/xmpp/service/HelloWorldService.java
@@ -2,10 +2,12 @@ package test.xmpp.service;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.ws.BindingType;
 
 @WebService(
         endpointInterface = "test.xmpp.service.HelloWorld", 
         serviceName = "HelloWorld")
+@BindingType(value="http://www.w3.org/2003/05/soap/bindings/HTTP/")
 public class HelloWorldService implements HelloWorld
 {
     public String sayHi(@WebParam(name="text")String text)
