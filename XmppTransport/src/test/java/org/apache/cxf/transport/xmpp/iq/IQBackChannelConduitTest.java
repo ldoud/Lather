f5100804eaa172f0781907d423c9a615ac529dec
Leon Doud <leon.doud@gmail.com>
2012-03-09 23:03:19 -0500

    Working on tests.

--- XmppTransport/src/test/java/org/apache/cxf/transport/xmpp/iq/IQBackChannelConduitTest.java
+++ XmppTransport/src/test/java/org/apache/cxf/transport/xmpp/iq/IQBackChannelConduitTest.java
@@ -18,20 +18,40 @@
  */
 package org.apache.cxf.transport.xmpp.iq;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IQBackChannelConduitTest {
-    
+
+    private String xmppServiceName = "localhost.localdomain";
+    private String xmppUsername = "user1";
+    private String xmppPassword = "user1";
+    private XMPPConnection xmppConnection;
+
     @Before
     public void setupXmppServer() {
         new ClassPathXmlApplicationContext("xmpp-embedded-broker.xml");
+
+        xmppConnection = new XMPPConnection(new ConnectionConfiguration(xmppServiceName, 61222));
+
+        try {
+            // Login to the XMMP server using the username
+            // and password from the configuration.
+            xmppConnection.connect();
+            xmppConnection.login(xmppUsername, xmppPassword);
+        } catch (XMPPException xmppError) {
+            Assert.fail("Couldn't login");
+        }
     }
-    
+
     @Test
     public void test() {
-        System.out.println("Hello World");
+        Assert.fail();
     }
-    
+
 }
